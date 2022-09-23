baad0b3497bf02f5171962265724354796c95fb1
jonathanpsantos <jonathan.santos194@etec.sp.gov.b>
2022-09-22 22:16:03 -0300

    criando card's view

---
+++ app/src/main/java/br/com/etecia/skoutapp/CardModeloActivity.java
@@ -0,0 +1,14 @@
+package br.com.etecia.skoutapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CardModeloActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.car_modelo_layout);
+    }
+}
---
+++ app/src/main/java/br/com/etecia/skoutapp/Card_MyStyleActivity.java
@@ -0,0 +1,14 @@
+package br.com.etecia.skoutapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Card_MyStyleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.card_my_style_layout);
+    }
+}
